7fd86d40780277bfaa4d916ed537a5c60e50f1c2
Edina Orban <Amarillia@users.noreply.github.com>
2019-08-17 20:47:56 +0100

    added basic layout and FreeCodeCamp script

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <h1>JavaScript calculator</h1>
+    <div id="drum-machine">
+      <div id="display">
+        <AppDrumPad msg="1" id="one"></AppDrumPad>
+        <AppDrumPad msg="2" id="two"></AppDrumPad>
+        <AppDrumPad msg="3" id="three"></AppDrumPad>
+        <AppDrumPad msg="+" id="add"></AppDrumPad>
+        <AppDrumPad msg="4" id="four"></AppDrumPad>
+        <AppDrumPad msg="5" id="five"></AppDrumPad>
+        <AppDrumPad msg="6" id="six"></AppDrumPad>
+        <AppDrumPad msg="-" id="subtract"></AppDrumPad>
+        <AppDrumPad msg="7" id="seven"></AppDrumPad>
+        <AppDrumPad msg="8" id="eight"></AppDrumPad>
+        <AppDrumPad msg="9" id="nine"></AppDrumPad>
+        <AppDrumPad msg="*" id="multiply"></AppDrumPad>
+        <AppDrumPad msg="=" id="equals"></AppDrumPad>
+        <AppDrumPad msg="0" id="zero"></AppDrumPad>
+        <AppDrumPad msg="." id="decimal"></AppDrumPad>
+        <AppDrumPad msg="/" id="divide"></AppDrumPad>
+        <AppDrumPad msg="Clear" id="clear"></AppDrumPad>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppDrumPad from "./components/AppDrumPad.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppDrumPad
+  },
+  mounted() {
+    let recaptchaScript = document.createElement("script");
+    recaptchaScript.setAttribute(
+      "src",
+      "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
+    );
+    document.head.appendChild(recaptchaScript);
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Bubbler+One&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  background-color: #262621;
+}
+
+#app {
+  font-family: "Bubbler One", sans-serif;
+  font-weight: 100;
+  text-align: center;
+}
+#drum-machine {
+  width: 80%;
+  margin: 0 auto;
+}
+
+#display {
+  display: grid;
+  grid-template-columns: 125px 125px 125px 125px;
+  grid-template-rows: 125px 125px 125px 125px 125px;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+  justify-content: center;
+  background-color: #5679a6;
+  border-radius: 25px;
+  padding: 15px;
+}
+
+h1 {
+  font-size: 50px;
+  margin: 25px;
+  color: #fff;
+}
+
+#clear {
+  width: 560px;
+}
+</style>
---
+++ src/components/AppDrumPad.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="drum-pad">
+    <button>{{msg}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppDrumPad",
+  props: {
+    msg: String,
+   }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.drum-pad {
+background:none;
+}
+
+button {
+  border: 1px solid #A6123A;
+  background-color: #A6123A;
+  font-size: 55px;
+  width: 125px;
+  height: 125px;
+  cursor: pointer;
+  border-radius: 25px;
+  color: #BFBDB0;
+}
+</style>
